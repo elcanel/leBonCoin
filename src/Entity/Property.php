d6dc99aa22f723bfac6afdea724bd7e7566e42c2
elcanel <51972275+elcanel@users.noreply.github.com>
2019-12-10 00:42:30 +0100

    17

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
@@ -38,6 +39,8 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * * @Assert\Length( max=7)
+     * * @Assert\Range(min=0)
      */
     private $prix;
 
